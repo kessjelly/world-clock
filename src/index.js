7fffa2e0bfe0f01c3e9992d7b3c7300ff0207533
kessjelly <jessicaskelly01@gmail.com>
2025-07-04 14:44:32 +1000

    Displayed correct time and date with java

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+function updateLocalTime() {
+  let localElement = document.querySelector("#local");
+  let localDateElement = localElement.querySelector(".date");
+  let localTimeElement = localElement.querySelector(".time");
+  let localTime = moment().tz("Australia/Brisbane");
 
+  localDateElement.innerHTML = moment().format("MMMM d, YYYY");
+  localTimeElement.innerHTML = localTime.format(
+    "HH:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateLocalTime();
+setInterval(updateLocalTime, 1000);
