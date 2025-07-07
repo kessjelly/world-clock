bc25c3871148475b30e0868373cf73d91b2a6a38
kessjelly <jessicaskelly01@gmail.com>
2025-07-07 18:03:56 +1000

    Added more code

--- src/index.js
+++ src/index.js
@@ -10,5 +10,26 @@ function updateLocalTime() {
   );
 }
 
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityTime = moment().tz(cityTimeZone);
+  let cityNameElement = cityTimeZone.replace("_", " ").split("/")[1];
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `  
+    <div class="city">
+      <div>
+          <h2>${cityNameElement}</h2>
+          <div class="date">${cityTime.format("MMMM D, YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format(
+          "HH:mm:ss [<small>]A[</small>]"
+        )}</div>
+      </div>
+    </div>`;
+}
+
 updateLocalTime();
 setInterval(updateLocalTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
